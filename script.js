ff89d28a79d79d7e56134e51a8811769333e8555
miladnazar <69940276+miladnazar@users.noreply.github.com>
2025-01-12 22:30:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Contact form functionality
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
     alert('Thank you for contacting Tax Whiz! We will get back to you shortly.');
