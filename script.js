fa1088d35a6511c1db397b997e1797e280e44c6b
miladnazar <69940276+miladnazar@users.noreply.github.com>
2025-01-18 13:31:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,22 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
-menuToggle.addEventListener('change', function () {
+menuToggle?.addEventListener('change', function () {
     if (menuToggle.checked) {
         menu.style.display = 'flex'; // Show menu on mobile
     } else {
         menu.style.display = 'none'; // Hide menu on mobile
     }
+});
+
+// Dropdown functionality
+const serviceItem = document.querySelector('#menu li:nth-child(2)'); // Target Services menu
+const dropdown = serviceItem.querySelector('.dropdown');
+
+serviceItem.addEventListener('mouseover', () => {
+    dropdown.style.display = 'block';
+});
+
+serviceItem.addEventListener('mouseout', () => {
+    dropdown.style.display = 'none';
 });
