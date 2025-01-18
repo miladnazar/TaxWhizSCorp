30303d5b3a423f421b561bb6d8efd3706e7b643b
miladnazar <69940276+miladnazar@users.noreply.github.com>
2025-01-17 22:36:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
-menuToggle.addEventListener('change', function() {
+menuToggle.addEventListener('change', function () {
     if (menuToggle.checked) {
         menu.style.display = 'flex'; // Show menu on mobile
     } else {
