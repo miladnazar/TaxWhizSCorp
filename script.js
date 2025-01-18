b71496cb2442746f8daae12fb619d4053b81fc3b
miladnazar <69940276+miladnazar@users.noreply.github.com>
2025-01-18 15:29:57 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Hamburger Menu Toggle
-const hamburger = document.getElementById('hamburger');
-const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
-
-hamburger.addEventListener('click', () => {
-    mobileMenuOverlay.classList.toggle('open'); // Toggle the 'open' class
-});
