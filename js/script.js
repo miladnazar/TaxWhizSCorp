5d77c0ee75e40ff19164e76bd53647e5be8c33ea
miladnazar <69940276+miladnazar@users.noreply.github.com>
2025-01-18 16:00:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 const hamburger = document.getElementById('hamburger');
 const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
 const menuLinks = document.querySelectorAll('#mobile-menu a');
-const servicesLink = document.getElementById('services-link');
-const servicesMenu = document.getElementById('services-submenu');
 
 // Toggle mobile menu visibility
 hamburger.addEventListener('click', () => {
@@ -15,10 +13,4 @@ menuLinks.forEach(link => {
     link.addEventListener('click', () => {
         mobileMenuOverlay.classList.remove('open');
     });
-});
-
-// Handle submenu toggle for "Services"
-servicesLink.addEventListener('click', (event) => {
-    event.preventDefault();
-    servicesMenu.classList.toggle('open');
 });
